e7eae534f115c814457166514d9f99c0b7a2a0c4
Chris Geihsler <chris@geihsler.net>
2014-12-21 08:53:34 -0500

    Removing users route.

--- bootstrap.js
+++ bootstrap.js
@@ -8,7 +8,6 @@ var faye         = require('faye');
 var http         = require('http');
 
 var routes = require('./routes/index');
-var users  = require('./routes/users');
 
 var app = express();
 
@@ -25,7 +24,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
